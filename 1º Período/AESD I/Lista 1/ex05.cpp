7c190a7fa01179fd75324c1e89386cd6919da589
Arley Augusto e Silva <67613403+arleynm@users.noreply.github.com>
2021-05-17 07:36:35 -0300

    Update ex05.cpp

---
+++ 1º Período/AESD I/Lista 1/ex05.cpp
@@ -0,0 +1,29 @@
+/* Faça um programa que calcule lucros em um investimento. O usuário deverá informar o valor
+   investido e o valor final que foi sacado. Ao final informar o lucro em reais e em percentual. 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	float valoriv,valorrb,formula,lucro;
+	
+	printf("Valor Investido:");
+	scanf("%f",&valoriv);
+	
+	printf("Valor Recebido:");
+	scanf("%f",&valorrb);
+	
+	lucro = valorrb - valoriv;
+	formula = (lucro * 100)/valoriv;
+	
+	
+	
+	printf("\nDados do Investimento:");
+	printf("\nValor Investido: R$ %.3f",valoriv);
+	printf("\nValor Recebido: R$ %.3f",valorrb);
+	printf("\nLucro: R$ %.2f",lucro);
+	printf("\nLucro Percentual: %.0f %%",formula);
+
+}
