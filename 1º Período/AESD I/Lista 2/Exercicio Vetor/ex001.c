1ccf3235f68f7a4f93b90c4eaac0384a828bf2b8
Arley Augusto e Silva <67613403+arleynm@users.noreply.github.com>
2021-05-17 13:59:07 -0300

    Update ex001.c

---
+++ 1º Período/AESD I/Lista 2/Exercicio Vetor/ex001.c
@@ -0,0 +1,21 @@
+/*  Faça um programa para calcular o fatorial de um número informado pelo usuário.
+    Obs. Por definição o fatorial de 0! = 1 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int fat, n;
+    printf("Insira um valor para o qual deseja calcular seu fatorial: ");
+    scanf("%d", &n);
+ 
+    for(fat = 1; n > 1; n = n - 1)
+    {    
+        fat = fat * n;
+    }
+ 
+    printf("\nFatorial calculado: %d", fat);
+    return 0;
+}
